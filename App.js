3c06f249842662606e53d16b533d225be50fb514
ganesh.babu <ganesh.babu@care.com>
2020-08-17 10:20:56 +0530

    APi change

--- App.js
+++ App.js
@@ -10,8 +10,8 @@ const util = require('./utils/util');
 
 const PORT = process.env.PORT || 5000;
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 // app.use(express.bodyParser());
 // app.use(express.json());       // to support JSON-encoded bodies
 // app.use(express.urlencoded({     // to support URL-encoded bodies
@@ -20,23 +20,20 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => res.render('pages/index'));
+app.get('/', (req, res) => res.json({"welcome_to":"plantorium"}));
 app.get('/cool', (req, res) => res.send(cool()))
 
 app.get('/:api/', (req, res) => {
   console.log(req.params);
   console.log(req.body.page);
   const url = util.getUrl(req.params.api);
+  console.log('------url---to fetch data------', url)
   fetch(url).then(response => response.json())
   .then(data => {
+    console.log( console.log('------url---------', url));
     res.json(data);
   });
 });
-
-
-getURL = () =>{
-  constants.API_HOST
-}
 
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
