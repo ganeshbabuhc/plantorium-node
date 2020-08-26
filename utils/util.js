e26f5fcb53e817fe5f2d50c1e0dfc9d4a8396ba6
ganesh.babu <ganesh.babu@care.com>
2020-08-26 16:05:10 +0530

    url breakdown change

--- utils/util.js
+++ utils/util.js
@@ -1,7 +1,7 @@
 const constants = require('./constants');
 module.exports = {
     getUrl: (url) => {
-        const params = url.split('&');
+        const params = url.split('?');
         let newUrl = `${constants.API_HOST}${params[0]}?token=${constants.API_TOKEN}`;
         if(params[1]) {
             newUrl+=`&${params[1]}`;
